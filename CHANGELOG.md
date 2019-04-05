e978e6d31c42017519b35ad228825e1ecdb31181
desconexo <feericles@gmail.com>
2019-04-05 10:42:52 -0300

    Minor maintenance fixes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.0.1+2
+Minor maintenance fixes
+
 ## 0.0.1+1
 Example correction
 
--- lib/highlight_text.dart
+++ lib/highlight_text.dart
@@ -2,13 +2,11 @@ library highlight_text;
 
 import 'package:flutter/services.dart';
 
-
 export 'src/HighlightedWord.dart';
 export 'src/TextHighlight.dart';
 
 class HighlightText {
-  static const MethodChannel _channel =
-      const MethodChannel('highlight_text');
+  static const MethodChannel _channel = const MethodChannel('highlight_text');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
